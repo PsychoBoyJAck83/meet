7f094c4d0fffe17cc6f5569fe047d3d907198a9e
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-07-26 19:45:58 +0200

    4.4 end of exercise

---
+++ src/components/EventList.js
@@ -0,0 +1,13 @@
+import Event from "./Event";
+// src/components/EventList.js
+
+const EventList = ({ events }) => {
+  return (
+    <ul id="event-list">
+      {events
+        ? events.map((event) => <Event key={event.id} event={event} />)
+        : null}
+    </ul>
+  );
+};
+export default EventList;
