69ce1808a7add2ba2417b67ac1bc8341449edf9d
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-08-13 02:08:40 +0200

    Sets errorAlert text when invalid input

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,10 +1,18 @@
 import React from "react";
-const NumberOfEvents = ({ currentNOE, setCurrentNOE }) => {
+const NumberOfEvents = ({ currentNOE, setCurrentNOE, setErrorAlert }) => {
   const handleInputChanged = (event) => {
+    let errorText = "";
     const NumberOfEventsInputValue = parseInt(event.target.value, 10);
-    isNaN(NumberOfEventsInputValue)
-      ? setCurrentNOE(0)
-      : setCurrentNOE(NumberOfEventsInputValue);
+    if (
+      isNaN(NumberOfEventsInputValue) ||
+      NumberOfEventsInputValue <= 0 ||
+      NumberOfEventsInputValue > 128
+    ) {
+      //Will only accept numbers
+      setCurrentNOE(0);
+      errorText = "Invalid Input. Please input a number between 1 and 128";
+    } else setCurrentNOE(NumberOfEventsInputValue);
+    setErrorAlert(errorText);
   };
 
   return (
